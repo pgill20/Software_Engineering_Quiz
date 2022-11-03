29b0ade8cee143618a3156b2c403f477cb7d6c96
Troy Peele <TroyPeele2@users.noreply.github.com>
2022-11-03 13:12:29 -0700

    resolve vercel

--- APIs/rankingsApi.py
+++ APIs/rankingsApi.py
@@ -1,54 +1,54 @@
-from google.cloud.sql.connector import Connector
-import sqlalchemy
-import pymysql
+# from google.cloud.sql.connector import Connector
+# import sqlalchemy
+# import pymysql
 
-# initialize Connector object
-connector = Connector()
+# # initialize Connector object
+# connector = Connector()
 
-# function to return the database connection
-def getconn() -> pymysql.connections.Connection:
-    conn: pymysql.connections.Connection = connector.connect(
-        "boxwood-valve-365621:us-west1:rankings",
-        "pymysql",
-        user="peelet",
-        password="467Ranking",
-        db="ranking"
-    )
-    return conn
+# # function to return the database connection
+# def getconn() -> pymysql.connections.Connection:
+#     conn: pymysql.connections.Connection = connector.connect(
+#         "boxwood-valve-365621:us-west1:rankings",
+#         "pymysql",
+#         user="peelet",
+#         password="467Ranking",
+#         db="ranking"
+#     )
+#     return conn
 
-def getRankingsByTestID(testId):
+# def getRankingsByTestID(testId):
 
-    pool = sqlalchemy.create_engine(
-        "mysql+pymysql://",
-        creator=getconn,
-    )
-    base_query = """
-    SELECT * FROM rankings WHERE TestID='{id}'
-    """
-    query = base_query.format(id=testId)
+#     pool = sqlalchemy.create_engine(
+#         "mysql+pymysql://",
+#         creator=getconn,
+#     )
+#     base_query = """
+#     SELECT * FROM rankings WHERE TestID='{id}'
+#     """
+#     query = base_query.format(id=testId)
 
-    with pool.connect() as db_conn:
-        result = db_conn.execute(query).fetchall()
-        return result
+#     with pool.connect() as db_conn:
+#         result = db_conn.execute(query).fetchall()
+#         return result
  
-    connector.close()
+#     connector.close()
 
-def insertTestResults(fullName, email, test, score, testId, employer):
-    pool = sqlalchemy.create_engine(
-        "mysql+pymysql://",
-        creator=getconn,
-    )
-    base_query = """
-    INSERT INTO rankings (FullName , Email , Test , Score , TestId , Employer) values ('{FullName}' , '{Email}' , '{Test}' , '{Score}' , '{TestId}' , '{Employer}')
-    """
-    query = base_query.format(FullName=fullName, Email=email, Test=test, Score=score, TestId=testId, Employer=employer)
+# def insertTestResults(fullName, email, test, score, testId, employer):
+#     pool = sqlalchemy.create_engine(
+#         "mysql+pymysql://",
+#         creator=getconn,
+#     )
+#     base_query = """
+#     INSERT INTO rankings (FullName , Email , Test , Score , TestId , Employer) values ('{FullName}' , '{Email}' , '{Test}' , '{Score}' , '{TestId}' , '{Employer}')
+#     """
+#     query = base_query.format(FullName=fullName, Email=email, Test=test, Score=score, TestId=testId, Employer=employer)
     
-    with pool.connect() as db_conn:
-        insert_stmt = sqlalchemy.text(
-        "INSERT INTO entries (guestName, content) values (:guestName, :content)",
-        )
-        db_conn.execute(query)
-    connector.close()
+#     with pool.connect() as db_conn:
+#         insert_stmt = sqlalchemy.text(
+#         "INSERT INTO entries (guestName, content) values (:guestName, :content)",
+#         )
+#         db_conn.execute(query)
+#     connector.close()
 
 # insertTestResults("Troy ", "peelet@oregon.edu", "Test 1", 8, "ID 255", "Google")
 
