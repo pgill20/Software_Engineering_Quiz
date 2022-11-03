86e8ce6d497334eb0f1b7bcae3afff04cbf8e48c
Troy Peele <TroyPeele2@users.noreply.github.com>
2022-11-03 16:02:23 -0700

    resolve vercel

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-from db_connector import connect_to_database, execute_query
+# from db_connector import connect_to_database, execute_query
 # from APIs import rankingsApi
 
 app = Flask(__name__)
@@ -19,15 +19,16 @@ def login():
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
-    db_connection = connect_to_database()
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        password = request.form['password']
-        query = "INSERT INTO Participants (first_name, last_name, password) VALUES (%s, %s, %s);"
-        data = (first_name, last_name, password)
-        execute_query(db_connection, query, data)
-        return redirect('/index')
+    # db_connection = connect_to_database()
+    # if request.method == 'POST':
+    #     first_name = request.form['first_name']
+    #     last_name = request.form['last_name']
+    #     password = request.form['password']
+    #     query = "INSERT INTO Participants (first_name, last_name, password) VALUES (%s, %s, %s);"
+    #     data = (first_name, last_name, password)
+    #     execute_query(db_connection, query, data)
+    #     return redirect('/index')
+    return render_template('register.html')
 
     # elif request.method == 'GET':
     # Under Construction
