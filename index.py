16c73f5f11792df61972371ab2cd6968b6eb1a8e
Troy Peele <TroyPeele2@users.noreply.github.com>
2022-11-03 10:28:21 -0700

    resolve index.py conflicts

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from db_connector import connect_to_database, execute_query
 from APIs import rankingsApi
 
 app = Flask(__name__)
@@ -6,7 +7,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    db_connection = connect_to_database()
+    query = "SELECT participant_id, first_name, password FROM Participants;"
+    result = execute_query(db_connection, query).fetchall()
+    return render_template('index.html', rows=result)
 
 
 @app.route('/login')
@@ -14,16 +18,26 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['POST', 'GET'])
 def register():
-    return render_template('register.html')
+    db_connection = connect_to_database()
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        password = request.form['password']
+        query = "INSERT INTO Participants (first_name, last_name, password) VALUES (%s, %s, %s);"
+        data = (first_name, last_name, password)
+        execute_query(db_connection, query, data)
+        return redirect('/index')
 
+    # elif request.method == 'GET':
+    # Under Construction
 
 @app.route('/rankings')
 def table():
     headings = ["Full Name" , "Email" , "Test" , "Score" , "Test ID" , "Employer", "Applicant ID"]
-    rankingData = rankingsApi.getRankingsByTestID("ID 255")
+    # index = 3
+    # rankingData = rankingsApi.getRankingsByTestID("ID 255")
+    # rankingData.sort(key = lambda x: x[index])
+    rankingData = ["Troy ", "peelet@oregon.edu", "Test 1", 8, "ID 255", "Google"]
     return render_template("table.html", headings=headings, data=rankingData)
-
-
-app.run(host='0.0.0.0', port=81)
