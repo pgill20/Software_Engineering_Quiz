fab4254c16dcd3d801166d25ef523e60a58e1f41
Troy Peele <TroyPeele2@users.noreply.github.com>
2022-11-03 13:16:33 -0700

    resolve vercel

--- index.py
+++ index.py
@@ -104,17 +104,17 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    db_connection = connect_to_database()
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        password = request.form['password']
-        query = "INSERT INTO Participants (first_name, last_name, password) VALUES (%s, %s, %s);"
-        data = (first_name, last_name, password)
-        execute_query(db_connection, query, data)
-        return redirect('/index')
+# @app.route('/register', methods=['POST', 'GET'])
+# def register():
+#     db_connection = connect_to_database()
+#     if request.method == 'POST':
+#         first_name = request.form['first_name']
+#         last_name = request.form['last_name']
+#         password = request.form['password']
+#         query = "INSERT INTO Participants (first_name, last_name, password) VALUES (%s, %s, %s);"
+#         data = (first_name, last_name, password)
+#         execute_query(db_connection, query, data)
+#         return redirect('/index')
 
 @app.route('/create_quiz')
 def create_quiz():
