3d1c8faaa226563106e69fd9e8ada4d8e5df047c
pgill20 <paramvirr.gill@gmail.com>
2022-11-10 19:19:49 -0500

    Commenting out Vick's section to try out Troy's work first.

--- index.py
+++ index.py
@@ -4,30 +4,30 @@ from db_connector import connect_to_database, execute_query
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    db_connection = connect_to_database()
-    query = "SELECT participant_id, first_name, password FROM Participants;"
-    result = execute_query(db_connection, query).fetchall()
-    return render_template('index.html', rows=result)
+# @app.route('/')
+# def index():
+#     db_connection = connect_to_database()
+#     query = "SELECT participant_id, first_name, password FROM Participants;"
+#     result = execute_query(db_connection, query).fetchall()
+#     return render_template('index.html', rows=result)
 
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+# @app.route('/login')
+# def login():
+#     return render_template('login.html')
 
 
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    db_connection = connect_to_database()
-    if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        password = request.form['password']
-        query = "INSERT INTO Participants (first_name, last_name, password) VALUES (%s, %s, %s);"
-        data = (first_name, last_name, password)
-        execute_query(db_connection, query, data)
-        return redirect('/index')
+# @app.route('/register', methods=['POST', 'GET'])
+# def register():
+#     db_connection = connect_to_database()
+#     if request.method == 'POST':
+#         first_name = request.form['first_name']
+#         last_name = request.form['last_name']
+#         password = request.form['password']
+#         query = "INSERT INTO Participants (first_name, last_name, password) VALUES (%s, %s, %s);"
+#         data = (first_name, last_name, password)
+#         execute_query(db_connection, query, data)
+#         return redirect('/index')
 
     # elif request.method == 'GET':
     # Under Construction
