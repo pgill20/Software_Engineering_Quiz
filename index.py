5890eb00da3e6224f2f90f4ac1d4074b9ed1e6e5
pgill20 <paramvirr.gill@gmail.com>
2022-11-20 23:54:53 -0500

    Added authentication files.

--- index.py
+++ index.py
@@ -1,37 +1,72 @@
-# from flask import Flask, render_template, request, redirect
-# from db_connector import connect_to_database, execute_query
+import json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
 
 app = Flask(__name__)
+app.secret_key = env.get("APP_SECRET_KEY")
 
 
-# @app.route('/')
-# def index():
-#     db_connection = connect_to_database()
-#     query = "SELECT participant_id, first_name, password FROM Participants;"
-#     result = execute_query(db_connection, query).fetchall()
-#     return render_template('index.html', rows=result)
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
+)
 
 
-# @app.route('/login')
-# def login():
-#     return render_template('login.html')
+# Controllers API
+@app.route("/")
+def home():
+    return render_template(
+        "home.html",
+        session=session.get("user"),
+        pretty=json.dumps(session.get("user"), indent=4),
+    )
 
-@app.route('/')
-def index():
-    return 'Web App with Python Flask!'
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
 
 
-# @app.route('/register', methods=['POST', 'GET'])
-# def register():
-#     db_connection = connect_to_database()
-#     if request.method == 'POST':
-#         first_name = request.form['first_name']
-#         last_name = request.form['last_name']
-#         password = request.form['password']
-#         query = "INSERT INTO Participants (first_name, last_name, password) VALUES (%s, %s, %s);"
-#         data = (first_name, last_name, password)
-#         execute_query(db_connection, query, data)
-#         return redirect('/index')
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
 
-    # elif request.method == 'GET':
-    # Under Construction
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://"
+        + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=env.get("PORT", 3000))
