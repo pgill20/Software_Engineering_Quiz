8aa46cfb6a952c72e29e8574033ba4f2cd45217d
pgill20 <paramvirr.gill@gmail.com>
2022-11-23 15:16:57 -0500

    Deleting old unnecessary files.

--- quiz_DDM.sql
+++
@@ -1,9 +0,0 @@
---get all attributes of participants to populate Players table
-SELECT * FROM Participants;
-
---remove participant
-DELETE FROM Participants WHERE participant_id = :participant_id;
-
---add NEW player
-INSERT INTO Participants(first_name, last_name, password)
-VALUES(:participant_id, :first_name, :last_name, :password)
