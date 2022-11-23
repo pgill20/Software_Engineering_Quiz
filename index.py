14744daf51aa3d8e0f898e4ddca74dec82dc0dd0
pgill20 <paramvirr.gill@gmail.com>
2022-11-23 16:15:31 -0500

    Added authentication and removed unnecessary templating files.

--- index.py
+++ index.py
@@ -1,43 +1,84 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, session, url_for
 from google.cloud.sql.connector import Connector
-import sqlalchemy
-import pymysql
 import datetime
 import json
-# from db_connector import connect_to_database, execute_query
 from APIs import rankingsApi
 from sendemail import sendEmail
 from quizAPI import *
 
+# For User Authentication
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
 app = Flask(__name__)
+app.secret_key = env.get("APP_SECRET_KEY")
 
-# @app.route('/')
-# def index():
-# #     db_connection = connect_to_database()
-# #     query = "SELECT participant_id, first_name, password FROM Participants;"
-# #     result = execute_query(db_connection, query).fetchall()
-#     return render_template('index.html')
+oauth = OAuth(app)
 
-# @app.route('/')
-# def login():
-#     return render_template('login.html')
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
+)
+
+# Controllers API
 
 
-# @app.route('/register', methods=['POST', 'GET'])
-    # db_connection = connect_to_database()
-    # if request.method == 'POST':
-    #     first_name = request.form['first_name']
-    #     last_name = request.form['last_name']
-    #     password = request.form['password']
-    #     query = "INSERT INTO Participants (first_name, last_name, password) VALUES (%s, %s, %s);"
-    #     data = (first_name, last_name, password)
-    #     execute_query(db_connection, query, data)
-    #     return redirect('/index')
+@app.route("/")
+def home():
+    return render_template(
+        "home.html",
+        session=session.get("user"),
+        pretty=json.dumps(session.get("user"), indent=4),
+    )
+
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://"
+        + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
+
 
 @app.route('/create_quiz')
 def create_quiz():
-    # This route will allow employers to create their quiz. 
+    # This route will allow employers to create their quiz.
     return render_template('create_quiz.html')
+
 
 @app.route('/submit_quiz', methods=["POST"])
 def submit_quiz():
@@ -54,6 +95,7 @@ def submit_quiz():
     sendEmail(email, subjectInfo, testID)
     return redirect(url_for('index'), code=302)
 
+
 @app.route('/quiz')
 def take_quiz():
     # This route is taken from an email link to a particular quiz
@@ -66,6 +108,7 @@ def take_quiz():
     #quiz = [["multipleChoice", "question", "A", "B", "C", "D"]]
     #employer = "Dan"
     return render_template('take_quiz.html', q=quiz, t=testid, e=employer)
+
 
 @app.route('/submit_quiz_answers', methods=["POST"])
 def submit_quiz_answers():
@@ -80,47 +123,18 @@ def submit_quiz_answers():
     insertTestResults(fullName, email, score, testid, employer)
     return redirect(url_for('index'), code=302)
 
-# @app.route('/')
-# def index():
-# #     db_connection = connect_to_database()
-# #     query = "SELECT participant_id, first_name, password FROM Participants;"
-# #     result = execute_query(db_connection, query).fetchall()
-#     return render_template('index.html')
-
-# @app.route('/')
-# def login():
-#     return render_template('login.html')
-
-
-# @app.route('/register', methods=['POST', 'GET'])
-# def register():
-#     # db_connection = connect_to_database()
-#     # if request.method == 'POST':
-#     #     first_name = request.form['first_name']
-#     #     last_name = request.form['last_name']
-#     #     password = request.form['password']
-#     #     query = "INSERT INTO Participants (first_name, last_name, password) VALUES (%s, %s, %s);"
-#     #     data = (first_name, last_name, password)
-#     #     execute_query(db_connection, query, data)
-#     #     return redirect('/index')
-#     return render_template('register.html')
-
-#     # elif request.method == 'GET':
-#     # Under Construction
-
-@app.route('/')
-def index():
-    return "homepage for the Engineer"
 
 @app.route('/rankings')
 def table():
-    headings = ["Full Name" , "Email" , "Test" , "Score" , "Test ID" , "Employer", "Applicant ID"]
+    headings = ["Full Name", "Email", "Test", "Score",
+                "Test ID", "Employer", "Applicant ID"]
     index = 3
 
     # NEED TO UTILIZE A TEST ID FOR GAINING INFORMATION
     rankingData = rankingsApi.getRankingsByTestID("ID 255")
-    rankingData.sort(key = lambda x: x[index], reverse=True)
+    rankingData.sort(key=lambda x: x[index], reverse=True)
     return render_template("table.html", headings=headings, data=rankingData)
+
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
@@ -131,23 +145,3 @@ if __name__ == '__main__':
     # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
     # App Engine itself will serve those files as configured in app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-
-# @app.route('/login')
-# def login():
-#     return render_template('login.html')
-
-
-# @app.route('/register', methods=['POST', 'GET'])
-# def register():
-#     db_connection = connect_to_database()
-#     if request.method == 'POST':
-#         first_name = request.form['first_name']
-#         last_name = request.form['last_name']
-#         password = request.form['password']
-#         query = "INSERT INTO Participants (first_name, last_name, password) VALUES (%s, %s, %s);"
-#         data = (first_name, last_name, password)
-#         execute_query(db_connection, query, data)
-#         return redirect('/index')
-
-    # elif request.method == 'GET':
-    # Under Construction
